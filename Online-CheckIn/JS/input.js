cfff1f3bf995f32bf0f2868e398bc6f999fd93eb
25phamhaiyen <145901112+25phamhaiyen@users.noreply.github.com>
2024-11-08 21:21:13 +0700

    up

--- Online-CheckIn/JS/input.js
+++ Online-CheckIn/JS/input.js
@@ -20,24 +20,18 @@ function showPopup(popupId) {
   
   // Hàm kiểm tra thông tin
   function CheckThongTin() {
-    const name = document.getElementById("Ten").value.trim();
     const lastname = document.getElementById("Ho").value.trim();
     const id = document.getElementById("MaDatCho").value.trim();
   
-    const nameCheck = /^[A-Za-z]+$/;
     const lastnameCheck = /^[A-Za-z]+$/;
     const idCheck = /^[A-Za-z0-9]+$/;
   
     // Kiểm tra trường hợp thiếu thông tin
-    if (!name || !lastname || !id) {
+    if ( !lastname || !id) {
       showPopup('overlay'); // Hiển thị popup thiếu thông tin
     }
     // Kiểm tra trường hợp sai định dạng
-    else if (!nameCheck.test(name) || !lastnameCheck.test(lastname) || !idCheck.test(id)) {
+    else if (!lastnameCheck.test(lastname) || !idCheck.test(id)) {
       showPopup('saiDinhDang'); // Hiển thị popup sai định dạng
-    }
-    // Nếu hợp lệ
-    else {
-      alert("Thông tin hợp lệ, thực hiện tìm kiếm.");
     }
   }  
